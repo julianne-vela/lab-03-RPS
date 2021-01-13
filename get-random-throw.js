a2a85c0fee733ed4ca816e24d167b4a214272a81
Julianne Vela <cjvela@outlook.com>
2021-01-13 15:19:37 -0800

    created didUserWin function and tested player rock vs computer variable outcomes

--- get-random-throw.js
+++ get-random-throw.js
@@ -1,9 +1,17 @@
-
-
 export function getRandomThrow(compThrow) {
     if (compThrow === 1) {
         return 'rock'
     } else if (compThrow === 2) {
         return 'paper'
     } return 'scissors';
+}
+
+export function didUserWin(player, computer) {
+    if (player === 'rock' && computer === 'rock') {
+        return 'draw'
+    } else if (player === 'rock' && computer === 'paper') {
+        return 'lose'
+    } else if (player === 'rock' && computer === 'scissors') {
+        return 'win'
+    }
 }
--- test/example.test.js
+++ test/example.test.js
@@ -1,9 +1,13 @@
 // IMPORT MODULES under test here:
-import { getRandomThrow } from '../get-random-throw.js';
+import {
+    getRandomThrow,
+    didUserWin
+}
+    from '../get-random-throw.js';
 
 const test = QUnit.test;
 
-test('it should return rock if provided 1', (expect) => {
+test('it should return rock, paper, or scissors when provided 1, 2, & 3, respectively', (expect) => {
     const expected = 'rock';
     const expected2 = 'paper';
     const expected3 = 'scissors';
@@ -15,4 +19,28 @@ test('it should return rock if provided 1', (expect) => {
     expect.equal(actual, expected);
     expect.equal(actual2, expected2);
     expect.equal(actual3, expected3);
+});
+
+test('It should display draw if given two identical throws', (expect) => {
+    const expected = 'draw';
+
+    const actual = didUserWin('rock', 'rock');
+
+    expect.equal(actual, expected);
+});
+
+test('It should display lose if player is rock and computer is paper', (expect) => {
+    const expected = 'lose';
+
+    const actual = didUserWin('rock', 'paper');
+
+    expect.equal(actual, expected);
+});
+
+test('It should display win if player is rock and computer is scissors', (expect) => {
+    const expected = 'win';
+
+    const actual = didUserWin('rock', 'scissors');
+
+    expect.equal(actual, expected);
 });
