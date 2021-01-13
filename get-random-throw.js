1393707821ebb1a51bebfbebdba4395ad050d930
Julianne Vela <cjvela@outlook.com>
2021-01-13 15:30:04 -0800

    rewrote function with win results and tested each possible outcome

--- get-random-throw.js
+++ get-random-throw.js
@@ -7,11 +7,11 @@ export function getRandomThrow(compThrow) {
 }
 
 export function didUserWin(player, computer) {
-    if (player === 'rock' && computer === 'rock') {
-        return 'draw'
-    } else if (player === 'rock' && computer === 'paper') {
-        return 'lose'
-    } else if (player === 'rock' && computer === 'scissors') {
+    if (player === 'rock' && computer === 'scissors') {
+        return 'win'
+    } else if (player === 'paper' && computer === 'rock') {
+        return 'win'
+    } else if (player === 'scissors' && computer === 'paper') {
         return 'win'
     }
 }
